4102266f8ec7ab9174315e6b714c9b9e348a0352
Harsha Vardhan <harshavardhan1617@gmail.com>
2025-03-03 21:23:46 +0530

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 import solid from "@astrojs/solid-js";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
   integrations: [solid()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
